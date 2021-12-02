da545e41feb59753b07ff37fe76ffea7ff2438a8
M Yahya <ya7yaz@gmail.com>
2021-12-02 11:20:37 +0100

    files already unlocked

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -eu
 
 echo "$GIT_CRYPT_KEY" | base64  -d > /tmp/git-crypt-key
 
-git-crypt unlock /tmp/git-crypt-key
+# git-crypt unlock /tmp/git-crypt-key
 
-rm /tmp/git-crypt-key
+# rm /tmp/git-crypt-key
 
 node /opt/project/dist/index.js
